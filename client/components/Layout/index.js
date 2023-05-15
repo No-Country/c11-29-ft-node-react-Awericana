5c3648bbac5b20774424ed9d66b14fdd9d9f7576
Lázaro Sánchez <lazarosanchezjs@gmail.com>
2023-05-15 17:48:54 -0300

    Fix: cleanup default nextjs markup

---
+++ client/components/Layout/index.js
@@ -0,0 +1,11 @@
+import {Roboto} from "next/font/google"
+
+const roboto = Roboto({ weight: ['300', '400'], subsets: ["latin"] })
+
+export function Layout ({children}) {
+  return (
+    <main className={`${roboto.className}`}>
+      {children}
+    </main>
+  )
+}
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,17 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: 'rgba(34, 131, 112, 1)',
+        secondary: 'rgba(119, 181, 169, 1)',
+        black: 'rgba(0, 0, 0, 1)',
+        grayish: 'rgba(62, 62, 62, 1)',
+        white: 'rgba(255, 255, 255, 1)'
       },
+      fontWeight: {
+        light: '300',
+        regular: '400'
+      },
+      fontSize: {
+        
+      }
     },
   },
   plugins: [],
