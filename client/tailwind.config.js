dfc6127a00ff04641940c76678da8a853c3becb3
Lázaro Sánchez <lazarosanchezjs@gmail.com>
2023-05-16 20:57:09 -0300

    add: tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -12,14 +12,35 @@ module.exports = {
         secondary: 'rgba(119, 181, 169, 1)',
         black: 'rgba(0, 0, 0, 1)',
         grayish: 'rgba(62, 62, 62, 1)',
-        white: 'rgba(255, 255, 255, 1)'
+        white: 'rgba(255, 255, 255, 1)',
+        red: 'rgba(215, 0, 0, 1)'
       },
       fontWeight: {
         light: '300',
         regular: '400'
       },
       fontSize: {
-
+        big: '20px',
+        medium: '18px',
+        normal: '16px',
+        small: '15px',
+        smaller: '14px',
+        tiny: '12px'
+      },
+      width: {
+        icon: '48px'
+      },
+      boxShadow: {
+        normal: '0px 4px 4px rgba(0, 0, 0, 0.25);'
+      },
+      margin: {
+        layoutSides: '0 16px',
+        extra: '138px',
+        bigger: '48px',
+        big: '40px',
+        normal: '32px',
+        medium: '24px',
+        small: '16px'
       }
     }
   },
