1f75e88cb3d99b2c75bba4b41e5fb3634bfbefd6
Heberth Lopez <heberth.lopez.19@gmail.com>
2023-05-16 18:51:44 -0500

    Feat: Added Signup Page with styles

--- client/components/Layout/index.js
+++ client/components/Layout/index.js
@@ -1,10 +1,10 @@
-import { Roboto } from 'next/font/google'
+import { Inter } from 'next/font/google'
 
-const roboto = Roboto({ weight: ['300', '400'], subsets: ['latin'] })
+const inter = Inter({ weight: ['300', '400'], subsets: ['latin'] })
 
 export function Layout ({ children }) {
   return (
-    <main className={`${roboto.className}`}>
+    <main className={`${inter.className}`}>
       {children}
     </main>
   )
---
+++ client/pages/auth/signup.js
@@ -0,0 +1,18 @@
+import { Layout } from '@/components/Layout'
+import Image from 'next/image'
+import Logo from '../../public/assets/logo-big.svg'
+import SignupForm from '@/components/SignupForm'
+
+function Signup () {
+  return (
+    <Layout>
+      <section className='max-w-5xl mx-auto flex flex-col h-screen items-center p-8'>
+        <Image src={Logo} width={240} height={120}/>
+        <h1 className='my-8 bg-gradient-to-r from-green-400 via-emerald-500 to-green-600 bg-clip-text text-transparent text-3xl sm:text-3xl md:text-4xl md:ml-16 font-black self-start'> Crea tu cuenta</h1>
+        <SignupForm />
+      </section>
+    </Layout>
+  )
+}
+
+export default Signup
