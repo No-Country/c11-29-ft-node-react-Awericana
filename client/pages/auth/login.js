d03405f050a89b57fe1a159501d72dd980177890
Heberth Lopez <heberth.lopez.19@gmail.com>
2023-05-16 12:10:04 -0500

    Fixed styles import

--- client/pages/auth/login.js
+++ client/pages/auth/login.js
@@ -7,7 +7,7 @@ export default function Login () {
       <section className="flex h-screen justify-center">
         <img src='#' alt='' />
         <div className=" border-green-500 border h-[550px] flex flex-col justify-center items-center">
-          <h3 className="mt-10 ¿">Iniciar Sesión</h3>
+          <h3 className="mt-10">Iniciar Sesión</h3>
           <div className="flex  mb-5">
             <p>¿Aún no tienes cuenta?</p>
             <button>
