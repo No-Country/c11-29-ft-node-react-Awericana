72d6047bdb4434e36ca3ff1142820aea43d440f6
Lázaro Sánchez <lazarosanchezjs@gmail.com>
2023-05-18 00:36:00 -0300

    Fix: signin form

--- client/pages/auth/signin.js
+++ client/pages/auth/signin.js
@@ -1,18 +1,19 @@
 import { Layout } from '@/components/Layout'
 import { LoginForm } from '@/components/LoginForm'
+import { Primary } from '@/components/Buttons/Primary'
 
 export default function Signin () {
   return (
     <Layout>
       <section className="flex h-screen justify-center">
         <img src='#' alt='' />
-        <div className=" border-green-500 border h-[550px] flex flex-col justify-center items-center">
+        <div className="h-[550px] flex flex-col justify-center items-center">
           <h3 className="mt-10">Iniciar Sesión</h3>
-          <div className="flex  mb-5">
+          <div className="flex mb-5">
             <p>¿Aún no tienes cuenta?</p>
-            <button>
+            <Primary>
               <a className="text-blue-500">Registrate</a>
-            </button>
+            </Primary>
           </div>
           <LoginForm />
         </div>
--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -6,7 +6,7 @@ import { SignupForm } from '@/components/SignupForm'
 function Signup () {
   return (
     <Layout>
-      <section className='max-w-5xl mx-auto flex flex-col h-screen items-center p-8 h-full'>
+      <section className='max-w-5xl mx-auto flex flex-col h-screen items-center p-8'>
         <Image src={Logo} width={240} height={120}/>
         <h1 className='mt-8 my-4 text-lg leading-tight sm:text-lg md:text-3xl md:ml-16 self-start font-normal'>Registrate</h1>
         <p className='text-xs font-normal self-start mb-5'>Completa los siguientes datos:</p>
