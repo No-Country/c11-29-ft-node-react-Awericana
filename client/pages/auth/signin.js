a21a3b542f4ebc4aa8e84ad35735426bfe68c239
Lázaro Sánchez <lazarosanchezjs@gmail.com>
2023-05-18 15:36:31 -0300

    Fix: input error

--- client/pages/auth/signin.js
+++ client/pages/auth/signin.js
@@ -15,12 +15,12 @@ export default function Signin () {
           </nav>
       </header>
 
-      <div className='lg:flex justify-center m-auto lg:p-8'>
+      <div className='lg:flex justify-center w-full lg:p-8 lg:gap-20'>
         <Image src={'/assets/gato.jpg'} width={450} height={550} alt='Gato' className='hidden lg:inline-block object-right-top mt-extra object-contain' />
-        <section className="p-8 lg:p-0 max-w-5xl m-auto lg:m-0 flex-1 h-fit">
-          <h1 className='mt-8 my-4 text-lg leading-tight sm:text-lg md:text-3xl md:ml-16 lg:ml-[12%] self-center font-normal'>Inicio Sesion</h1>
-          <div className='text-xs font-normal self-start md:ml-16 lg:ml-[12%] md:text-sm mb-5'><p className='inline-block'>¿Aún no tienes cuenta?</p><Link href={'/auth/signup'} className='text-black ml-4 underline cursor-pointer'>Registrate</Link></div>
-          <div className='flex justify-center w-full'>
+        <section className="max-w-screen-sm p-8 lg:p-0 m-auto lg:m-0 flex-1 h-fit">
+          <h1 className='mt-8 my-4 text-lg leading-tight sm:text-lg md:text-3xl self-center font-normal'>Inicio Sesion</h1>
+          <div className='text-xs font-normal self-start md:text-sm mb-5'><p className='inline-block'>¿Aún no tienes cuenta?</p><Link href={'/auth/signup'} className='text-black ml-4 underline cursor-pointer'>Registrate</Link></div>
+          <div className='flex justify-center w-full max-w-screen-md'>
             <LoginForm />
           </div>
         </section>
--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -14,7 +14,7 @@ function Signup () {
             </Link>
           </nav>
       </header>
-      <section className='max-w-5xl mx-auto flex flex-col h-fit items-center p-8'>
+      <section className='max-w-screen-sm lg:max-w-5xl m-auto flex flex-col h-fit items-center p-8'>
         <h1 className='my-4 text-lg leading-tight sm:text-lg md:text-3xl self-start font-normal'>Registrate</h1>
         <p className='text-xs font-normal self-start md:text-sm mb-5'>Completa los siguientes datos:</p>
         <SignupForm />
