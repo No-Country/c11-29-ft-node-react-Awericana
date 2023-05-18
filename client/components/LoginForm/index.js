a63442b741c8dddaa9396f813a82bfe03818a412
jooseariias <arijoose@gmail.com>
2023-05-17 23:19:49 -0300

    estilos login

--- client/components/LoginForm/index.js
+++ client/components/LoginForm/index.js
@@ -1,7 +1,5 @@
 import { useInputValue } from '@/hooks/useInputValue'
-import { Input } from '@/components/Input'
 import { useEffect } from 'react'
-import { Primary } from '@/components/Buttons/Primary'
 import { Secondary } from '@/components/Buttons/Secondary'
 
 export function LoginForm () {
@@ -18,15 +16,16 @@ export function LoginForm () {
   }
 
   return (
-    <form className="w-[400px]  mt-10 flex flex-col" onSubmit={handleSubmit}>
-      <Input placeholder='Email' type={'email'} label={'Ingresa tu e-mail'} value={emailValue} onChange={emailOnChange} error={emailError} />
-      <Input placeholder='Contraseña' type={'password'} label={'Ingresa tu contraseña'} value={passwordValue} onChange={passwordOnChange} error={passwordError} />
-
-      <a className="ml-18 underline  mb-5">¿Olvidaste Tu Contraseña?</a>
-      <Primary>Iniciar Sesion</Primary>
-      <Secondary>Ingresar con Google</Secondary>
-      <Secondary>Ingresar con Facebook</Secondary>
-
+    <>
+    <form className="w-[380px]  mt-2 flex flex-col items-center justify-center" onSubmit={handleSubmit}>
+    <h3 className='text-3xl text-center mt-10 mb-10'>Inicio Sesion</h3>
+     <div><input className='border-green-400 border rounded-lg w-[339px] py-[8px] pl-4 mb-6' placeholder='Email' type={'email'} label={'Ingresa tu e-mail'} value={emailValue} onChange={emailOnChange} /></div>
+     <div><input className='border-green-400 border rounded-lg w-[339px] py-[8px] pl-4' placeholder='Contraseña' type={'password'} label={'Ingresa tu contraseña'} value={passwordValue} onChange={passwordOnChange} /></div>
+      <button className='m-10 bg-green-700 py-4  rounded-lg px-6  text-current' value="summit">Iniciar Sesion</button>
+      <a className="ml-18 underline   mb-5">¿Olvidaste Tu Contraseña?</a>
+     <div><Secondary>Ingresar con Google</Secondary></div>
+     <div><Secondary>Ingresar con Facebook</Secondary></div>
     </form>
+    </>
   )
 }
