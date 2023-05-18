449a7f709bf0190b231f8b22de62666598378f92
jooseariias <arijoose@gmail.com>
2023-05-18 01:17:15 -0300

    estilos formlogin

--- client/pages/auth/signin.js
+++ client/pages/auth/signin.js
@@ -1,22 +1,13 @@
 import { Layout } from '@/components/Layout'
 import { LoginForm } from '@/components/LoginForm'
-import { Primary } from '@/components/Buttons/Primary'
-
+import Header from '@/components/Header'
 export default function Signin () {
   return (
     <Layout>
+      <Header/>
       <section className="flex h-screen justify-center">
-        <img src='#' alt='' />
-        <div className="h-[550px] flex flex-col justify-center items-center">
-          <h3 className="mt-10">Iniciar Sesión</h3>
-          <div className="flex mb-5">
-            <p>¿Aún no tienes cuenta?</p>
-            <Primary>
-              <a className="text-blue-500">Registrate</a>
-            </Primary>
-          </div>
+      <div><img src='#' alt='' /></div>
           <LoginForm />
-        </div>
       </section>
     </Layout>
   )
