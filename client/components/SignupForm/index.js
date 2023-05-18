2368c04e3f72c40390b4e4e46c7b832378f05aea
Heberth Lopez <heberth.lopez.19@gmail.com>
2023-05-18 01:15:25 -0500

    Feat: Modified label styles

--- client/components/SignupForm/index.js
+++ client/components/SignupForm/index.js
@@ -1,21 +1,37 @@
 import { Input } from '@/components/Input'
 import { useInputValue } from '@/hooks/useInputValue'
+import { useState } from 'react'
 
 export function SignupForm () {
   const { passwordValue, passwordOnChange, error: passwordError } = useInputValue()
+  const [dataForm, setDataForm] = useState({
+    nombre: '',
+    apellido: '',
+    email: '',
+    password: '',
+    password_confirmation: '',
+    fecha_nacimiento: ''
+  })
+
+  function handleChange (e) {
+    const { name, value } = e.target
+    setDataForm({ ...dataForm, [name]: value })
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault()
+    console.log(dataForm)
   }
 
   return (
-    <form className='flex flex-col gap-4 w-full md:w-9/12'>
-      <Input type='text' placeholder={'Nombre y apellido*'} label={'Nombre y apellido*'} />
-      <Input type='email' placeholder={'Email*'} label={'Email*'}/>
-      <Input type='email' placeholder={'Repetir email*'} label={'Repetir email*'}/>
-      <Input type={'password'} placeholder={'Contraseña*'} label={'Contraseña*'} value={passwordValue} onChange={passwordOnChange} error={passwordError} />
-      <Input type={'password'} placeholder={'Repetir contraseña*'} label={'Repetir contraseña*'} value={passwordValue} onChange={passwordOnChange} error={passwordError} />
-      <button onSubmit={handleSubmit} className='my-6 self-end py-3 px-6 bg-secondary shadow-lg rounded-xl text-white font-md text-lg hover:scale-105 hover:bg-primary transition'>
+    <form className='flex flex-col gap-4 w-full md:w-9/12' onSubmit={handleSubmit}>
+      <Input name={'nombre'} type='text' placeholder={'Nombre(s)*'} label={'Nombre(s)*'} onChange={handleChange}/>
+      <Input name={'apellidos'} type='text' placeholder={'Apellido(s)*'} label={'Apellido(s)*'} onChange={handleChange} />
+      <Input name={'email'} type='email' placeholder={'Email*'} label={'Email*'} onChange={handleChange}/>
+      <Input name={'password'} type={'password'} placeholder={'Contraseña*'} label={'Contraseña*'} value={passwordValue} onChange={passwordOnChange} error={passwordError} />
+      <Input name={'password_confirmation'} type={'password'} placeholder={'Repetir contraseña*'} label={'Repetir contraseña*'} value={passwordValue} onChange={passwordOnChange} error={passwordError} />
+      <Input name={'fecha_nacimiento'} type={'date'} placeholder={'Fecha de nacimiento*'} label={'Fecha de nacimiento*'} onChange={handleChange} />
+      <button type='submit' className='my-6 self-end py-3 px-6 bg-secondary shadow-lg rounded-xl text-white font-md text-lg hover:scale-105 hover:bg-primary transition'>
         Registrarse
       </button>
     </form>
--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -2,14 +2,17 @@ import { Layout } from '@/components/Layout'
 import Image from 'next/image'
 import Logo from '@/public/assets/logobig.svg'
 import { SignupForm } from '@/components/SignupForm'
+import Link from 'next/link'
 
 function Signup () {
   return (
     <Layout>
       <section className='max-w-5xl mx-auto flex flex-col h-screen items-center p-8 h-full'>
-        <Image src={Logo} width={240} height={120}/>
+        <Link href={'/'}>
+          <Image src={Logo} width={240} height={120}/>
+        </Link>
         <h1 className='mt-8 my-4 text-lg leading-tight sm:text-lg md:text-3xl md:ml-16 self-start font-normal'>Registrate</h1>
-        <p className='text-xs font-normal self-start mb-5'>Completa los siguientes datos:</p>
+        <p className='text-xs font-normal self-start md:ml-16 md:text-sm mb-5'>Completa los siguientes datos:</p>
         <SignupForm />
       </section>
     </Layout>
