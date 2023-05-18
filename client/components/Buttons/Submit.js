f8de28b70cdf54c0c9c02aceeaaad74f695fe491
Lázaro Sánchez <lazarosanchezjs@gmail.com>
2023-05-18 12:31:26 -0300

    Add: submit button disabled

--- client/components/Buttons/Submit.js
+++ client/components/Buttons/Submit.js
@@ -1,6 +1,6 @@
-export function Submit ({ children, center = false }) {
+export function Submit ({ children, center = false, disabled = false }) {
   return (
-      <button type='submit' className={`my-6 ${center ? 'self-middle ' : 'self-end'} py-3 px-6 bg-secondary shadow-lg rounded-xl text-white font-md text-lg hover:scale-105 hover:bg-primary transition`}>
+      <button type='submit' disabled={disabled} className={`my-6 ${center ? 'self-middle ' : 'self-end'} py-3 px-6 bg-secondary shadow-lg rounded-xl text-white font-md text-lg hover:scale-105 hover:bg-primary transition`}>
         {children}
       </button>
   )
--- client/components/LoginForm/index.js
+++ client/components/LoginForm/index.js
@@ -4,14 +4,28 @@ import { useFormFields } from '@/hooks/useFormFields'
 import { Submit } from '../Buttons/Submit'
 import Link from 'next/link'
 import { Secondary } from '@/components/Buttons/Secondary'
+import { useState } from 'react'
 
 export function LoginForm () {
+  const initialError = {
+    email: false,
+    password: false
+  }
   const { data, handleChange } = useFormFields({
     email: '',
     password: ''
   })
+  const [error, setError] = useState(initialError)
 
   const handleSubmit = (e) => {
+    setError(initialError)
+    if (!data.email) {
+      setError(prev => ({ ...prev, email: 'El email es inválido' }))
+    } else if (!data.password) {
+      setError(prev => ({ ...prev, password: 'La contraseña es inválida' }))
+    } else {
+    }
+
     e.preventDefault()
     console.log(data)
   }
@@ -21,7 +35,7 @@ export function LoginForm () {
         <Input name='email' placeholder='Ingresa tu e-mail' type={'email'} label={'Ingresa tu e-mail'} onChange={handleChange} />
         <Input name='password' placeholder='Ingresa tu contraseña' type={'password'} label={'Ingresa tu contraseña'} onChange={handleChange} />
         <Link href={'#'} className="ml-18 underline cursor-pointer text-black">¿Olvidaste Tu Contraseña?</Link>
-        <Submit center={true} >INICIAR SESIÓN</Submit>
+        <Submit disabled={Boolean(error.email || error.password)} center={true} >INICIAR SESIÓN</Submit>
         <footer className='flex flex-col w-full md:w-9/12 m-auto'>
           <Secondary>Ingresar con Google</Secondary>
           <Secondary>Ingresar con Facebook</Secondary>
