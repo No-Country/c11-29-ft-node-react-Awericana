366b000a129e0bdb3873f210cd061540fc14f0c9
jooseariias <arijoose@gmail.com>
2023-05-17 22:11:41 -0300

    header falta lo de las imagenes xd

---
+++ client/components/Search/Search.js
@@ -0,0 +1,8 @@
+
+export default function Search () {
+  return (
+    <div>
+      <input className="border border-slate-200 text-gray-700 md:w-[624px] md:h-[40px] w-[229px] h-[32px] text-sm outline-none shadow-md p-3 rounded-xl focus:border-green-500 focus:outline-none focus:ring-1 focus:ring-green-400 focus:ring-opacity-50 placeholder:text-sm placeholder:text-slate-400 border-green-400  " type="search" placeholder="Buscar" />
+    </div>
+  )
+}
