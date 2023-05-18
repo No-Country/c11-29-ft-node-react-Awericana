79e6192aababbf2a7153d76549aafbe8a400d9ac
Heberth Lopez <heberth.lopez.19@gmail.com>
2023-05-18 11:30:08 -0500

    Fixed desktop view for SignUp Page

--- client/components/Buttons/Submit.js
+++ client/components/Buttons/Submit.js
@@ -1,6 +1,6 @@
 export function Submit ({ children, center = false }) {
   return (
-      <button type='submit' className={`my-6 ${center ? 'self-middle ' : 'self-end'} py-3 px-6 bg-secondary shadow-lg rounded-xl text-white font-md text-lg hover:scale-105 hover:bg-primary transition`}>
+      <button type='submit' className={`my-6 ${center ? 'self-center' : 'self-end'} lg:w-[28rem] lg:h-14 py-3 px-6 bg-secondary shadow-lg rounded-xl text-white font-md text-lg hover:scale-105 hover:bg-primary transition`}>
         {children}
       </button>
   )
--- client/components/Form/index.js
+++ client/components/Form/index.js
@@ -1,6 +1,6 @@
 export function Form ({ children, onSubmit }) {
   return (
-  <form className='flex flex-col gap-4 w-full md:w-9/12' onSubmit={onSubmit}>
+  <form className='flex flex-col gap-4 w-full' onSubmit={onSubmit}>
     {children}
   </form>
   )
