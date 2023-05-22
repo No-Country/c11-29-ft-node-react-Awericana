1ed209789fc418c32266f32c8146bb3be542f4cb
Seba Luna <ramon.sebastian.luna@gmail.com>
2023-05-22 17:20:58 -0300

    feat: created component category

---
+++ client/components/Category/index.js
@@ -0,0 +1,20 @@
+import Image from 'next/image';
+import card from '@/public/assets/img-category/category.png'
+
+const Category = ({name = 'sin nombre'}) => {
+    return (
+        <div className='inline-block text-center m-6'>
+            <Image
+                className='rounded-full'
+                src={card}
+                width={104}
+                height={104}
+                alt="categoria"
+            />
+            <h2 className='mt-1'>{name}</h2>
+        </div>
+        
+    )
+}
+
+export default Category;
