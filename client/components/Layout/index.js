9e0bc5116c48270e1674a86a137b4b7e6a3feae6
Seba Luna <ramon.sebastian.luna@gmail.com>
2023-05-22 14:16:56 -0300

    feat: created cards

--- client/components/Layout/index.js
+++ client/components/Layout/index.js
@@ -1,6 +1,7 @@
 import { Roboto } from 'next/font/google'
 import { Footer } from '@/components/Footer'
 import Banner from '../Banner'
+import Card from '../Card'
 
 const roboto = Roboto({ weight: ['300', '400'], subsets: ['latin'] })
 
@@ -9,6 +10,22 @@ export function Layout ({ children }) {
     <main className={`${roboto.className}`}>
       {children}
       <Banner/>
+      
+      <div className='flex flex-wrap justify-center'>
+        <Card
+            precio={400}
+            titulo={'Remera'}
+            talleMedidas={'M'}
+        />
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+      </div>
+
       <Footer/>
     </main>
   )
