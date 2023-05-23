bad81c43ce70bc1934b7fe4fe11b7fe50cabb20d
Seba Luna <ramon.sebastian.luna@gmail.com>
2023-05-23 19:08:15 -0300

    fix: moved banner, card and categories to home page

--- client/components/Layout/index.js
+++ client/components/Layout/index.js
@@ -1,8 +1,5 @@
 import { Roboto } from 'next/font/google'
 import { Footer } from '@/components/Footer'
-import Banner from '../Banner'
-import Card from '../Card'
-import Categories from '../Category/Categories'
 
 const roboto = Roboto({ weight: ['300', '400'], subsets: ['latin'] })
 
@@ -10,29 +7,6 @@ export function Layout ({ children }) {
   return (
     <main className={`${roboto.className}`}>
       {children}
-      <Banner/>
-      
-      <div className='p-4'>
-        <section>
-            <Categories/>
-        </section>
-
-        <section className='flex flex-wrap justify-center'>
-            <Card
-                precio={400}
-                titulo={'Remera'}
-                talleMedidas={'M'}
-            />
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-        </section>
-      </div>
-
       <Footer/>
     </main>
   )
