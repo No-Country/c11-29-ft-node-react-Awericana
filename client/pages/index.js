95b793dc8232301cc263d867c01d49ba3f67ff55
jooseariias <arijoose@gmail.com>
2023-05-22 21:04:00 -0300

    titulos

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,12 @@
 import { Header } from '@/components/Header'
 import { Layout } from '@/components/Layout'
-
+import Head from 'next/head'
 export default function Home () {
   return (
     <Layout>
+        <Head>
+        <title>Inicio</title>
+      </Head>
       <Header />
     </Layout>
   )
