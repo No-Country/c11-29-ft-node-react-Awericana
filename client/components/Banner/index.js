891bbc95a20e19f73cb85d09e5cc3abc982d0854
Lázaro Sánchez <lazarosanchezjs@gmail.com>
2023-05-24 20:07:40 -0300

    add: direccion routes

--- client/components/Banner/index.js
+++ client/components/Banner/index.js
@@ -1,27 +1,27 @@
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import Image from 'next/image';
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import Image from 'next/image'
 
-/*importar las imagenes que van a ir en el Slider*/
+/* importar las imagenes que van a ir en el Slider */
 import banner1 from '@/public/assets/img-banner/banner1.png'
 import banner2 from '@/public/assets/img-banner/banner2.png'
 import banner3 from '@/public/assets/img-banner/banner3.png'
 
 const Banner = () => {
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        speed: 1000,
-        autoplaySpeed: 6000,
-        cssEase: "linear"
-    }
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 1000,
+    autoplaySpeed: 6000,
+    cssEase: 'linear'
+  }
 
-    return (
+  return (
         <>
             <Slider {...settings} className='w-full'>
                 <Image
@@ -42,10 +42,9 @@ const Banner = () => {
                     height={640}
                     alt="Picture of the author"
                 />
-            </Slider>  
+            </Slider>
         </>
-    )
+  )
 }
-  
-export default Banner;
-  
+
+export default Banner
