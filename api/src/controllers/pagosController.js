4c618b416b609a3e3ae32c400c2db3b189568317
Sebastian Torreiro <sebastiantorreiro@gmail.com>
2023-05-25 17:47:43 -0300

    mercadopago funcionando 90%

--- api/src/controllers/pagosController.js
+++ api/src/controllers/pagosController.js
@@ -1,5 +1,5 @@
 const mercadopago = require("mercadopago");
-const { Usuario_publicacion, Publicacion, Usuario } = require("../db");
+// const { Usuario_publicacion, Publicacion, Usuario } = require("../db");
 
 mercadopago.configure({
   access_token:
@@ -15,79 +15,104 @@ async function getUrlPago(req, res) {
   //   });
 
   //   let items = carrito.productos.maps({});
-
-  let preference = {
-    back_urls: {
-      success: "http://localhost:3001/pagos/success",
-      pending: "http://localhost:3001/pagos/pending",
-      failure: "http://localhost:3001/pagos/failure",
-    },
-    items: [
-      {
-        // id,
-        // description,
-        // picture_url,
-        currency_id: "ARS",
-        title: "Mi producto",
-        unit_price: 100,
-        quantity: 1,
+  try {
+    const result = await mercadopago.preferences.create({
+      back_urls: {
+        success: "http://localhost:3001/pagos/success",
+        pending: "http://localhost:3001/pagos/pending",
+        failure: "http://localhost:3001/pagos/failure",
       },
-    ],
-    notification_url:
-      "hasddasdadsok-free.app/pagos/notificar",
-  };
-
-  mercadopago.preferences
-    .create(preference)
-    .then(function (response) {
-      res.send(`<a href=${response.body.init_point}> IR A PAGAR </a>`);
+      items: [
+        {
+          // id,
+          // description,
+          // picture_url,
+          currency_id: "ARS",
+          title: "Mi producto",
+          unit_price: 100,
+          quantity: 1,
+        },
+      ],
+      notification_url:
+        "https://136a-2802-8010-9915-be01-1dad-f436-6ce2-6787.ngrok-free.app/pagos/notificar",
     })
-    .catch(function (error) {
-      console.log(error);
-    });
-}
 
-async function notificarYConfirmarPago(req, res) {
-  const { body, query } = req;
-  const topic = query.topic;
-  var merchantOrders;
-  switch (topic) {
-    case "payment":
-      const paymentId = query.id;
-      // console.log('getting paymentId:', paymentId)
-      const payment = await mercadopago.payment.findById(paymentId);
-      merchantOrders = await mercadopago.merchant_orders.findById(
-        payment.body.order.id
-      );
-      break;
-    case "merchant_order":
-      const orderId = query.id;
-      console.log("gettin merchant order", orderId);
-      merchantOrders = await mercadopago.merchant_orders.findById(orderId);
-      break;
+
+    res.json(result.body.init_point);
+  } catch (error) {
+    return res.status(500).json({ message: "Something goes wrong" });
   }
   
-  console.log(merchantOrders)
+}
+ 
 
-  var paidAmount = 0;
+//   mercadopago.preferences
+//     .create(preference)
+//     .then(function (response) {
+//       res.send(`<a href=${response.body.init_point}> IR A PAGAR </a>`);
+//     })
+//     .catch(function (error) {
+//       console.log(error);
+//     });
 
-  // merchantOrders.body.payments.forEach( payment  => {
-  //   if(payment.status === "approved"){
-  //       paidAmount += payment.transaction_amount;
-  //   }
-  // });
+async function notificarYConfirmarPago(req, res) {
+  try {
+    const payment = req.query;
+    const body = req.body
+    if (payment.type === "payment") {
+      const data = await mercadopago.payment.findById(payment["data.id"]);
+      console.log(data.response.status);
+      // pago rechazado ---> status = rejected
+      // pago pagofacil ---> status = pending
+      // pago aprobado ---> status = aproved
+      console.log('el pago se realizo')
+    }
 
 
-  if(paidAmount >= body.total_amount){
-    console.log('el pago se completo')
-  }else{
-    console.log('el pago no se completo')
+
+    res.sendStatus(204);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Something goes wrong" });
   }
-  res.send('terminado el pago')
+  // var merchantOrders;
+  // switch (topic) {
+  //   case "payment":
+  //     const paymentId = query.id;
+  //     // console.log('getting paymentId:', paymentId)
+  //     const payment = await mercadopago.payment.findById(paymentId);
+  //     merchantOrders = await mercadopago.merchant_orders.findById(
+  //       payment.body.order.id
+  //     );
+  //     break;
+  //   case "merchant_order":
+  //     const orderId = query.id;
+  //     console.log("gettin merchant order", orderId);
+  //     merchantOrders = await mercadopago.merchant_orders.findById(orderId);
+  //     break;
+  // }
+  
+  // console.log(merchantOrders)
+
+  // var paidAmount = 0;
+
+  // // merchantOrders.body.payments.forEach( payment  => {
+  // //   if(payment.status === "approved"){
+  // //       paidAmount += payment.transaction_amount;
+  // //   }
+  // // });
+
+
+  // if(paidAmount >= body.total_amount){
+  //   console.log('el pago se completo')
+  // }else{
+  //   console.log('el pago no se completo')
+  // }
+  // res.send('terminado el pago')
 
 
 
-  console.log(merchantOrders);
+  // console.log(merchantOrders);
 }
 
 module.exports = {
