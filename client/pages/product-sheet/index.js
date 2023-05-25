3cbb2c244b38339c81f0843c28230b6ff81d28cb
Seba Luna <ramon.sebastian.luna@gmail.com>
2023-05-25 02:13:36 -0300

    feat: Added the product section

--- client/pages/product-sheet/index.js
+++ client/pages/product-sheet/index.js
@@ -1,8 +1,11 @@
 import { Layout } from "@/components/Layout";
 import { Header } from "@/components/Header";
 import Image from "next/image";
-import producto from '@/public/assets/img-prod-vendedor/prod-vendedor.png'
-
+import producto from '@/public/assets/img-prod-vendedor/prod-vendedor.png';
+import banner from '@/public/assets/img-banner-ficha-mobile/banner-ficha-mobile.png';
+import { Primary } from "@/components/Buttons/Primary";
+import { Secondary } from "@/components/Buttons/Secondary";
+import { Submit } from "@/components/Buttons/Submit";
 
 const productSheet = () => {
     return (
@@ -11,10 +14,39 @@ const productSheet = () => {
             <main className="p-4">
                 <section id="producto">
                     <div>
-
+                        <Image
+                            src={banner}
+                            width={379}
+                            height={224}
+                            alt="banner-producto"
+                        />
+                    </div>
+                    <div className="flex mt-6">
+                        <div className="w-2/4">
+                            <p className="text-2xl">Precio</p>
+                            <p className="text-xl">Título</p>
+                            <p className="text-xl">Talle</p>
+                            <p className="text-sm">Ver tabla de talles</p>   
+                        </div>
+                        <div className="w-2/4 text-right">
+                            <p>Agregar a favoritos</p>
+                        </div>
                     </div>
                     <div>
-
+                        <div>
+                            <p className="text-xl">Detalle</p>                            
+                        </div>
+                        <div>
+                            <p className="text-xl">Vendedor</p>                     
+                        </div>
+                    </div>
+                    <div className="flex flex-col">
+                        <Submit>
+                            COMPRAR
+                        </Submit>
+                        <Secondary>
+                            Agregar al carrito
+                        </Secondary>
                     </div>
                 </section>
                 <section id="productos-del-mismo-vendedor">
