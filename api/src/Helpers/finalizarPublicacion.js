810ebd33f15beb6f27d8271612ecdac8ddc4cb88
Sebastian Torreiro <sebastiantorreiro@gmail.com>
2023-05-26 20:28:09 -0300

    fix en mp y helpers

--- api/src/Helpers/finalizarPublicacion.js
+++ api/src/Helpers/finalizarPublicacion.js
@@ -1,21 +1,22 @@
-const Publicacion = require("../models/Publicacion")
+const { Publicacion } = require("../db.js");
 
 const finalizarPublicacion = async (publicacionId, compradorId) => {
-    const publicacion = await Publicacion.findByPk(publicacionId);
+  const publicacion = await Publicacion.findByPk(publicacionId);
 
-    if(!publicacion){
-         return res.status(404).json(`La publicación con el ID: ${publicacionId} no existe.`);
-    }
+  if (!publicacion) {
+    return res
+      .status(404)
+      .json(`La publicación con el ID: ${publicacionId} no existe.`);
+  }
 
-    const cambios = {
-        compradorId,
-        estado: 'finalizada' 
-    }
+  const cambios = {
+    compradorId,
+    estado: "finalizada",
+  };
 
-    publicacion.update(cambios);
-}
+  publicacion.update(cambios);
+};
 
 module.exports = {
-    finalizarPublicacion
-}
-
+  finalizarPublicacion,
+};
