9e3cb9217295a89dde95f4ffd693ee407d31c4bd
Adhemir Sabino <adhemirsabino@gmail.com>
2023-05-27 16:04:59 -0500

    se agrego default value a calificacion

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -13,31 +13,26 @@ const productsRoute = require("./producto");
 const publicaciones = require("./publicaciones");
 const carrito = require("./carrito");
 const rutaBaseDeDatos = require("./routeCargaBaseDeDatos");
-const tipoProducto = require("./tipoProducto");
-
+const categoria = require("./categoria");
 const authRouter = require("./auth");
 const favoritos = require("./favoritos");
-const usuario = require("./usuario");
-
-const pagos = require("./pagosRoutes");
-
-const busquedas = require("./busquedas");
 const authTerceros = require("./authTerceros.js");
+const usuario = require("./usuario");
+const pagos = require("./pagosRoutes");
+const busquedas = require("./busquedas");
+const posventa = require("./posventa");
 
 router.use("/producto", productsRoute);
 router.use("/auth", authRouter);
-
-router.use("/auth", authTerceros);
-
 router.use("/publicaciones", publicaciones);
 router.use("/carrito", carrito);
 router.use("/cargaBaseDeDatos", rutaBaseDeDatos);
-router.use("/tipoDeProducto", tipoProducto);
+router.use("/categoria", categoria);
 router.use("/favoritos", favoritos);
-
+router.use("/auth", authTerceros);
 router.use("/pagos", pagos);
-
 router.use("/busqueda", busquedas);
+router.use("/posventa", posventa);
 router.use("/usuario", usuario);
 
 module.exports = router;
