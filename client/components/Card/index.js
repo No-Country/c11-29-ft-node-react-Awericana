9db388f54dfa2fcc557eee3cf4e1e67b6ea021c8
Lázaro Sánchez <lazarosanchezjs@gmail.com>
2023-05-30 01:00:16 -0300

    fix/images

--- client/components/Card/index.js
+++ client/components/Card/index.js
@@ -1,31 +1,30 @@
-import Image from 'next/image';
+import Image from 'next/image'
 import card from '@/public/assets/img-card/card.png'
 
-
 const Card = ({
-    precio = "No especificado",
-    titulo = "No especificado",
-    talleMedidas = "No especificado"    
+  precio = 'No especificado',
+  titulo = 'No especificado',
+  talleMedidas = 'No especificado',
+  imgSrc = card
 }) => {
-
-    return (
+  return (
         <div className='inline-flex flex-col'>
             <div className='m-3'>
                 <Image
                     className='rounded-lg'
-                    src={card}
+                    src={imgSrc}
                     width={297}
                     height={154}
                     alt="producto"
                 />
             </div>
-            <div className='mx-3'> 
+            <div className='mx-3'>
                 <p>${precio}</p>
                 <p>{titulo}</p>
                 <p>{talleMedidas}</p>
             </div>
         </div>
-    )
+  )
 }
 
-export default Card;
+export default Card
