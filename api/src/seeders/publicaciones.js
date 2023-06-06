793f8f423526d5b98c7f6e7b40065b7bfaf82eb0
Marco Romero <marcod.romero@gmail.com>
2023-06-05 21:05:36 -0300

    Corregir passwords de seeders y agregar publicaciones

--- api/src/seeders/publicaciones.js
+++ api/src/seeders/publicaciones.js
@@ -1,5 +1,26 @@
 let publicaciones = [
-    {
-
+    {    
+        "usuarioId": 2,
+        "titulo": "Remera pokemon",
+        "descripcion": "Remera con estampado de Pikachu y sus amigos para niños.",
+        "precio": 20,
+        "talleId": 5,
+        "personaId": 3,
+        "productoId": 13,
+        "imagenPortada": "https://memoestampados.com/wp-content/uploads/2022/07/D_823731-MLU50898059962_072022-F.jpg"
+    },
+    {    
+        "usuarioId": 2,
+        "titulo": "Pantalon de jean gris",
+        "descripcion": "Pantalon con poco uso para hombre.",
+        "precio": 10,
+        "talleId": 17,
+        "personaId": 1,
+        "productoId": 8,
+        "imagenPortada": "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/231/400/products/d_701612-mla51094003746_082022-o-f43d8e3103670f150616608407925290-240-0.jpg"
     }
 ];
+
+module.exports = {
+    publicaciones,
+  };
