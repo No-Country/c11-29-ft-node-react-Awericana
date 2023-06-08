e4c90b839311cfa5b4a351537e8709b243d45a07
Lázaro Sánchez <lazarosanchezjs@gmail.com>
2023-06-08 19:43:36 -0300

    fix:api

--- api/src/app.js
+++ api/src/app.js
@@ -19,6 +19,8 @@ server.use(
     name: "session",
     keys: [process.env.JWT_SECRET],
     maxAge: 24 * 60 * 60 * 100,
+    sameSite: 'none',
+    secure: true
   })
 );
 
@@ -29,7 +31,7 @@ server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", '*'); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", process.env.FRONT_URL); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
