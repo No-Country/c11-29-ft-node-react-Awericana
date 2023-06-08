a69a9751c2b001963b23d28803a757ccbacb231f
Sebastian Torreiro <sebastiantorreiro@gmail.com>
2023-06-08 02:40:42 -0300

    update styles

--- client/components/ShipingTracking/index.js
+++ client/components/ShipingTracking/index.js
@@ -1,9 +1,8 @@
-import React from 'react';
+import React from "react";
 
-export function TrackingComponent({ estado }){
+export function TrackingComponent({ estado }) {
+  const seguimientoEnvioHeight = "295px";
 
-  const seguimientoEnvioHeight = "285px";
-  
   const containerStyle = {
     height: seguimientoEnvioHeight,
   };
@@ -11,20 +10,39 @@ export function TrackingComponent({ estado }){
     <div className="flex">
       <div>
         <div className="flex flex-col items-center content-start">
-        <div className={`w-4 h-4 rounded-full bg-primary z-10`}></div>
-        <div className={`-mt-1 w-2 h-32 bg-${estado === 'Empacando' ? 'grayish' : 'primary' }`}></div>
-        <div className={`-mt-1 w-4 h-4 rounded-full bg-${estado === 'Empacando' ? 'grayish' : 'primary' } z-10`}></div>
-        <div className={`-mt-2 w-2 h-32 bg-${estado === 'Recibido' ? 'primary' : 'grayish'}`}></div>
-        <div className={`-mt-1 w-4 h-4 rounded-full bg-${estado === 'Recibido' ? 'primary' : 'grayish'} z-10`}></div>
+          <div className={`w-5 h-5 rounded-full bg-primary z-10`}></div>
+          <div
+            className={`-mt-1 w-2 h-32 bg-${
+              estado === "Empacando" ? "grayish" : "primary"
+            }`}
+          ></div>
+          <div
+            className={`-mt-1 w-5 h-5 rounded-full bg-${
+              estado === "Empacando" ? "grayish" : "primary"
+            } z-10`}
+          ></div>
+          <div
+            className={`-mt-2 w-2 h-32 bg-${
+              estado === "Entregado" ? "primary" : "grayish"
+            }`}
+          ></div>
+          <div
+            className={`-mt-1 w-5 h-5 rounded-full bg-${
+              estado === "Entregado" ? "primary" : "grayish"
+            } z-10`}
+          ></div>
+        </div>
       </div>
-      </div>
-      <div className="ml-4">
-        <div className="flex flex-col items-center justify-between" style={containerStyle}>
-          <p className="text-sm">Empacando</p>
-          <p className="text-sm">En camino</p>
-          <p className="text-sm">Recibido</p>
+      <div className="ml-8">
+        <div
+          className="flex flex-col items-center justify-between"
+          style={containerStyle}
+        >
+          <p className="text-xl font-medium text-primary">Empacando</p>
+          <p className={`text-xl font-medium text-${estado === "Empacando" ? "grayish" : "primary"}`}>En camino</p>
+          <p className={`text-xl font-medium text-${estado === "Entregado" ? "primary" : "grayish"}`}>Entregado</p>
         </div>
       </div>
     </div>
   );
-};
+}
