250bd572da9684077765f3e273c535b2ad33f528
Lázaro Sánchez <lazarosanchezjs@gmail.com>
2023-06-09 12:18:47 -0300

    fix:eslint

--- api/src/routes/carrito.js
+++ api/src/routes/carrito.js
@@ -8,6 +8,17 @@ const { obtenerCarrito, agregarAlCarrito, quitarDelCarrito } = require("../contr
 
 const router = Router();
 
+router.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", process.env.URL_FRONT); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
 router.get('/' , [
     authMiddleware
 ], obtenerCarrito );
